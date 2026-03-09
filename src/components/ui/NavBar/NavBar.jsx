9c88dbbae77f6080560753782b42180974456a3e
Luis Pinto <dapin46@hotmail.com>
2026-03-09 16:56:38 -0500

    add: keep search params

--- src/components/ui/NavBar/NavBar.jsx
+++ src/components/ui/NavBar/NavBar.jsx
@@ -1,13 +1,21 @@
-import { NavLink } from 'react-router';
+import { NavLink, useSearchParams } from 'react-router';
 import { menuOptions } from './menu/menu-options';
 import './NavBar.css';
 
 export const NavBar = () => {
+  const [searchParams] = useSearchParams();
+
   return (
     <ul className="navbar-nav ms-auto me-3">
       {menuOptions.map((option) => (
         <li key={option.path} className="nav-item">
-          <NavLink className="nav-link text-light" to={option.path}>
+          <NavLink
+            className="nav-link text-light"
+            to={{
+              pathname: option.path,
+              search: searchParams.toString(),
+            }}
+          >
             {option.text}
           </NavLink>
         </li>

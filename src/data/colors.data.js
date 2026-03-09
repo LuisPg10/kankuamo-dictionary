1e08dccb461cf87c88a36658c2fd7535e1aa7562
Luis Pinto <dapin46@hotmail.com>
2026-03-09 18:03:56 -0500

    add: sounds for all colors in english and kankuamo

--- src/data/colors.data.js
+++ src/data/colors.data.js
@@ -1,7 +1,7 @@
 const colorImage = '/assets/images/user.png';
 const imagePath = '/assets/images/colors';
 const kankuamoSoundPath = '/assets/sounds/colors/kankuamo';
-const englishSoundPath = '/assets/sounds/colors/kankuamo';
+const englishSoundPath = '/assets/sounds/colors/english';
 
 export const colorsData = [
   {
@@ -9,63 +9,63 @@ export const colorsData = [
     spanish: 'Blanco',
     kankuamo: 'Bunchikönke',
     english: 'White',
-    kankuamoSound: '',
-    englishSound: '',
+    kankuamoSound: `${kankuamoSoundPath}/bunchikönke.webm`,
+    englishSound: `${englishSoundPath}/white.webm`,
   },
   {
     image: colorImage,
     spanish: 'Amarillo',
     kankuamo: 'Akänl',
     english: 'Yellow',
-    kankuamoSound: '',
-    englishSound: '',
+    kankuamoSound: `${kankuamoSoundPath}/akänl.webm`,
+    englishSound: `${englishSoundPath}/yellow.webm`,
   },
   {
     image: colorImage,
     spanish: 'Azul',
     kankuamo: 'Chusuasi',
     english: 'Blue',
-    kankuamoSound: '',
-    englishSound: '',
+    kankuamoSound: `${kankuamoSoundPath}/chusuasi.webm`,
+    englishSound: `${englishSoundPath}/blue.webm`,
   },
   {
     image: colorImage,
     spanish: 'Rojo',
     kankuamo: 'Amakua',
     english: 'Red',
-    kankuamoSound: '',
-    englishSound: '',
+    kankuamoSound: `${kankuamoSoundPath}/amakua.webm`,
+    englishSound: `${englishSoundPath}/red.webm`,
   },
   {
     image: colorImage,
     spanish: 'Anaranjado',
     kankuamo: 'Yikanka',
     english: 'Orange',
-    kankuamoSound: '',
-    englishSound: '',
+    kankuamoSound: `${kankuamoSoundPath}/yikanka.webm`,
+    englishSound: `${englishSoundPath}/orange.webm`,
   },
   {
     image: colorImage,
     spanish: 'Morado',
     kankuamo: 'Uözi',
     english: 'Purple',
-    kankuamoSound: '',
-    englishSound: '',
+    kankuamoSound: `${kankuamoSoundPath}/uözi.webm`,
+    englishSound: `${englishSoundPath}/purple.webm`,
   },
   {
     image: colorImage,
     spanish: 'Verde',
     kankuamo: 'Atahkáye',
     english: 'Green',
-    kankuamoSound: '',
-    englishSound: '',
+    kankuamoSound: `${kankuamoSoundPath}/atahkáye.webm`,
+    englishSound: `${englishSoundPath}/green.webm`,
   },
   {
     image: colorImage,
     spanish: 'Negro',
     kankuamo: 'Niakönke',
     english: 'Black',
-    kankuamoSound: '',
-    englishSound: '',
+    kankuamoSound: `${kankuamoSoundPath}/niakönke.webm`,
+    englishSound: `${englishSoundPath}/black.webm`,
   },
 ];
